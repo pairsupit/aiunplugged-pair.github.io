f6f1416a3b2ae8dc9a89eeff6ae5b995a0bd5086
Your Name <you@example.com>
2020-07-17 00:53:20 +0700

    Once again

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@ import App from './App.vue'
 import DragAndDrop from 'vue-drag-and-drop'
 import VueTour from 'vue-tour'
 import Vue2TouchEvents from 'vue2-touch-events'
+import VueAnalytics from 'vue-analytics'
 
 require('vue-tour/dist/vue-tour.css')
 
+Vue.use(VueAnalytics, {
+  id: 'UA-172818561-1'
+})
 Vue.config.productionTip = false
 Vue.use(DragAndDrop)
 Vue.use(VueTour)
 Vue.use(Vue2TouchEvents)
+
 
 new Vue({
   render: h => h(App)
